a91a83e7fb2701b860e025fd986c5ed3f97d2638
Luca <luca.renz@hotmail.com>
2024-11-20 21:23:38 +0100

    [LATIN-12] Format the project for linting

--- server/py/main.py
+++ server/py/main.py
@@ -1,15 +1,14 @@
+import asyncio
+import json
+import random
+
 from fastapi import FastAPI, Request, WebSocket, WebSocketDisconnect
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 
-import json
-import asyncio
-
-import server.py.hangman as hangman
 import server.py.battleship as battleship
-
-import random
+import server.py.hangman as hangman
 
 app = FastAPI()
 
@@ -29,6 +28,7 @@ async def get(request: Request):
 async def hangman_singleplayer(request: Request):
     return templates.TemplateResponse("game/hangman/singleplayer_local.html", {"request": request})
 
+
 @app.websocket("/hangman/singleplayer/ws")
 async def hangman_singleplayer_ws(websocket: WebSocket):
     await websocket.accept()
@@ -44,7 +44,8 @@ async def hangman_singleplayer_ws(websocket: WebSocket):
             words = json.load(fin)
         word_to_guess = random.choice(words)
 
-        state = hangman.HangmanGameState(word_to_guess=word_to_guess, phase=hangman.GamePhase.RUNNING, guesses=[], incorrect_guesses=[])
+        state = hangman.HangmanGameState(word_to_guess=word_to_guess, phase=hangman.GamePhase.RUNNING, guesses=[],
+                                         incorrect_guesses=[])
         game.set_state(state)
 
         while True:
@@ -153,7 +154,7 @@ async def battleship_singleplayer_ws(websocket: WebSocket):
             if state.phase == battleship.GamePhase.FINISHED:
                 break
 
-            #game.print_state()
+            # game.print_state()
 
             if state.idx_player_active == idx_player_you:
 
