5f35d42698833115231f5f5b6a71d25a71921d34
Alvaro Cervan <89271771+JackGraymer@users.noreply.github.com>
2024-12-10 00:23:25 +0100

    adds test card exchanged

--- test/test_dog.py
+++ test/test_dog.py
@@ -458,7 +458,31 @@ class TestDogBenchmark:
         actions = self.game_server.get_list_action()
         assert len(actions) == 1
 
+    def test_get_list_action_card_exchanged(self):
+        """Test get_list_action when a card has been exchanged"""
+        game_state = GameState(
+            cnt_player=4,
+            phase=GamePhase.RUNNING,
+            cnt_round=1,
+            bool_card_exchanged=False,
+            idx_player_started=0,
+            idx_player_active=0,
+            list_player=[
+                PlayerState(name='Player 1', list_card=[Card(rank='A', suit='x')], list_marble=[Marble(pos=1, is_save=False)]),
+                PlayerState(name='Player 2', list_card=[], list_marble=[]),
+                PlayerState(name='Player 3', list_card=[Card(rank='2', suit='w')], list_marble=[]),
+                PlayerState(name='Player 4', list_card=[], list_marble=[])
+            ],
+            list_card_draw=GameState.LIST_CARD.copy(),
+            list_card_discard=[],
+            card_active=None
+        )
+        self.game_server.set_state(game_state)
+        actions = self.game_server.get_list_action()
+        assert len(actions) > 0 # At least one action should be available
+
     
+
 
         
 # --- end of tests ---
